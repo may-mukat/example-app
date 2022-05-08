cc26809923c17341ba9510a472e259d2e2772d81
owner <owner@ownernoMac-mini.local>
2022-05-08 22:21:19 +0900

    assets=true

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,4 +9,4 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "htm", to: "https://cdn.esm.sh/v45/htm@3.1.0/es2021/htm.js"
 pin "react", to: "https://cdn.esm.sh/v45/react@17.0.2/es2021/react.js"
 pin "react-dom", to: "https://cdn.esm.sh/v45/react-dom@17.0.2/es2021/react-dom.js"
-pin_all_from "app/javascript/components", under: "components", preload: true
+pin_all_from "app/javascript/components", under: "components"
